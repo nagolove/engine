7e900af99f5a6108a0a6a6afee1aa840eeb30698
nagolove <dekar.own@gmail.com>
2020-12-10 01:06:21 +0300

    More shit.

---
+++ scenes/hst_reader/file-thread.lua
@@ -0,0 +1,14 @@
+﻿local arg = ...
+local inspect = require "inspect"
+local serpent = require "serpent"
+
+local fname = love.thread.getChannel("fname"):pop()
+
+local function firstRead()
+end
+
+local function secondRead()
+end
+
+while true do
+end
--- scenes/hst_reader/init.lua
+++ scenes/hst_reader/init.lua
@@ -1,8 +1,12 @@
 ﻿local struct = require "struct"
 local histPath = "c:/users/dekar/AppData/Roaming/MetaQuotes/Terminal/287469DEA9630EA94D0715D755974F1B/history/Alpari-Demo/EURUSD999.hst"
 local imgui = require "imgui"
+local thread
 
 local function init()
+    thread = love.thread.newThread("file-thread.lua")
+    love.thread.getChannel("fname"):push(histPath)
+    thread.start(1)
 end
 
 local function quit()
