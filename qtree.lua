f6559f56deacb3a0fc26ff2965b67d3c3f2c9114
nagolove <dekar.own@gmail.com>
2020-12-17 21:28:47 +0300

    Empty quadtree file.

---
+++ qtree.lua
@@ -0,0 +1,23 @@
+﻿--[[
+-- Класс запросов к прямоугольникам. Представляет собой quad-tree для плоскости.
+--]]
+
+local inspect = require "inspect"
+
+local QTree = {}
+QTree.__index = QTree
+
+function QTree.new()
+    local self = {}
+    return setmetatable({}, QTree)
+end
+
+function QTree:add(x, y, w, h)
+end
+
+function QTree:query(x, y)
+end
+
+return {
+    new = QTree.new,
+}
