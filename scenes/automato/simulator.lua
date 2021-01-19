0fd74953a87770a9cfafe89db4fe7b89102dbc1f
nagolove <dekar.own@gmail.com>
2021-01-19 06:11:29 +0300

    simulator.tl written.

--- scenes/automato/simulator.lua
+++ scenes/automato/simulator.lua
@@ -1,196 +1,262 @@
-local inspect = require "inspect"
-local serpent = require "serpent"
-require "external"
+local _tl_compat53 = ((tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3) and require('compat53.module'); local ipairs = _tl_compat53 and _tl_compat53.ipairs or ipairs; local math = _tl_compat53 and _tl_compat53.math or math; local pcall = _tl_compat53 and _tl_compat53.pcall or pcall; local string = _tl_compat53 and _tl_compat53.string or string; local table = _tl_compat53 and _tl_compat53.table or table;  CommonSetup = {}
+
+
+ SimulatorMode = {}
+
+
+
+
+
+local inspect = require("inspect")
+local serpent = require("serpent")
+
+
+require("love")
+require("external")
+require("log")
+require("mtschemes")
 
 local threads = {}
+
 local gridSize
 local mtschema
 
--- количество потоков
-local threadCount
 
--- режим работы - протяженный, пошаговый или стоп
-local mode = "stop" -- "step"
+local threadCount = -1
+
+
+local mode = "stop"
+
+ Statistic = {}
+
+
 
 local statistic = {}
 
 local function getDrawLists()
-  local list = {}
-  for k, v in pairs(threads) do
-    local sublist = love.thread.getChannel("data" .. k):demand()
-    for k1, v1 in pairs(sublist) do
-      table.insert(list, v1)
-    end
-  end
-  return list
+   local list = {}
+   for k, _ in ipairs(threads) do
+      local sublist = love.thread.getChannel("data" .. k):demand()
+      for _, v1 in ipairs(sublist) do
+         table.insert(list, v1)
+      end
+   end
+   return list
 end
 
 local function pushSync()
-  local syncChan = love.thread.getChannel("sync")
-  for i = 1, threadCount do
-    syncChan:push("sync")
-  end
+   local syncChan = love.thread.getChannel("sync")
+   local i = 1
+   while i < threadCount do
+      i = i + 1
+      syncChan:push("sync")
+   end
+
+
+
 end
 
 local function pushMsg2Threads(t)
-  for i = 1, threadCount do
-    love.thread.getChannel("msg" .. i):push(t)
-  end
+   for i = 1, threadCount do
+      love.thread.getChannel("msg" .. i):push(t)
+   end
 end
 
-local function stopThreadsClearChannels()
-  if #threads ~= 0 then
-    pushMsg2Threads("stop")
-    love.timer.sleep(0.05)
-    for i = 1, threadCount do
-      love.thread.getChannel("msg" .. i):clear()
-      love.thread.getChannel("data" .. i):clear()
-      love.thread.getChannel("setup" .. i):clear()
-      love.thread.getChannel("request" .. i):clear()
-    end
-  end
+local function sendStopClearChannels()
+   if #threads ~= 0 then
+      pushMsg2Threads("stop")
+      love.timer.sleep(0.05)
+      for i = 1, threadCount do
+         love.thread.getChannel("msg" .. i):clear()
+         love.thread.getChannel("data" .. i):clear()
+         love.thread.getChannel("setup" .. i):clear()
+         love.thread.getChannel("request" .. i):clear()
+      end
+   end
 end
+
+ CommonSetup = {}
+
+
+
+
+
+
 
 local function create(commonSetup)
-  --stopThreadsClearChannels()
-  threads = {}
-  
-  mode = "continuos"
 
-  threadCount = commonSetup.threadCount
-  print("threadCount", threadCount)
+   sendStopClearChannels()
 
-  gridSize = commonSetup.gridSize
-  mtschema = require "mtschemes"[threadCount]
-  if not mtschema then
-    error(string.format("Unsupported scheme for %d threads.", threadCount))
-  end
 
-  for i = 1, threadCount do
-    local ok, errmsg = pcall(function()
-        local setupName = "setup" .. i
-        love.thread.getChannel(setupName):push(commonSetup)
-        love.thread.getChannel(setupName):push(serpent.dump(mtschema[i]))
+   threadCount = commonSetup.threadCount
+   print("threadCount", threadCount)
 
-        local th = love.thread.newThread("scenes/automato/simulator-thread.lua")
-        table.insert(threads, th)
-        th:start(i)
-        local errmsg = th:getError()
-        if errmsg then
-          logfwarn("Thread %s", errmsg)
-        end
+   gridSize = commonSetup.gridSize
+
+   mtschema = require("mtschemes")[threadCount]
+
+
+   if not mtschema then
+      error(string.format("Unsupported scheme for %d threads.", threadCount))
+   end
+
+   for i = 1, threadCount do
+      local ok, errmsg = pcall(function()
+         local setupName = "setup" .. i
+         love.thread.getChannel(setupName):push(commonSetup)
+         love.thread.getChannel(setupName):push(serpent.dump(mtschema[i]))
+
+         local th = love.thread.newThread("scenes/automato/simulator-thread.lua")
+         table.insert(threads, th)
+         th:start(i)
+         local errmsg = th:getError()
+         if errmsg then
+
+            print("Thread %s", errmsg)
+         end
       end)
-    if not ok then
-      logferror("Error in creating thread %s", errmsg)
-    end
-  end
+      if not ok then
+         logferror("Error in creating thread %s", errmsg)
+      end
+   end
 
-  pushSync()
-  --love.timer.sleep(0.5)
+   pushSync()
 
-  for k, v in pairs(threads) do
-    print(v:getError())
-  end
 
-  local processorCount = love.system.getProcessorCount()
-  print("processorCount", processorCount)
+   print("threads", inspect(threads))
+   for _, v in ipairs(threads) do
+      print(v:getError())
+   end
+
+   local processorCount = love.system.getProcessorCount()
+   print("processorCount", processorCount)
 end
 
 
 local function getThreadsLog()
-  local logChan = love.thread.getChannel("log")
-  local msg = logChan:pop()
-  while msg do
-    print(msg[1], msg[2])
-    msg = logChan:pop()
-  end
+   local logChan = love.thread.getChannel("log")
+   local msg = logChan:pop()
+   while msg do
+      print(msg[1], msg[2])
+      msg = logChan:pop()
+   end
 end
 
 local function step()
-  if mode == "stop" then
-    return
-  end
+   if mode == "stop" then
+      return
+   end
 
-  local iterSum = 0
-  local iterChan = love.thread.getChannel("iter")
-  local value = iterChan:pop()
-  while value do
-    iterSum = iterSum + value
-    value = iterChan:pop()
-  end
-  statistic.iterAverage = iterSum / threadCount
-  pushSync()
+   local iterSum = 0
+   local iterChan = love.thread.getChannel("iter")
+   local value = iterChan:pop()
+   while value do
+      iterSum = iterSum + value
+      value = iterChan:pop()
+   end
+   statistic.iterAverage = iterSum / threadCount
+   pushSync()
 end
 
--- FIXME Как починить счетчик итераций при работе в несколько нитей?
+
 local function getIter()
-  return statistic.iterAverage
+
+
+
+
+
+
+
+   return 0
 end
 
--- возвращает номер нити многопоточной схемы по координатам 
+
 local function findThreadByPos(x, y)
-  local ix, iy = math.floor(x / gridSize), math.floor(y / gridSize)
-  local rx, ry = x % gridSize, y % gridSize
-  for k, v in pairs(mtschema) do
-  end
-  return nil
+   local ix, iy = math.floor(x / gridSize), math.floor(y / gridSize)
+   local rx, ry = x % gridSize, y % gridSize
+
+
+
+
+
+   return nil
 end
 
--- здеcь нужно определять в какой из потоков отправить запрос используя каналы
--- msg1, msg2, ...
--- функция - запрос для визуального отладчика
+
+
+
 local function getObject(x, y)
-  local threadNum = findThreadByPos(x, y)
+   local threadNum = findThreadByPos(x, y)
 
-  local chan = love.thread.getChannel("msg")
-  chan:push("getobject")
-  chan:push(x)
-  chan:push(y)
+   local chan = love.thread.getChannel("msg")
+   chan:push("getobject")
+   chan:push(x)
+   chan:push(y)
 
-  local sobject = love.thread.getChannel("request" .. threadNum):demand()
-  local objectfun, err = loadstring(sobject)
+   local sobject = love.thread.getChannel("request" .. threadNum):demand()
 
-  if err then
-    logferror("Could'not deserialize cell object %s", err)
-    return nil
-  end
 
-  return objectfun()
+   local objectfun, err = serpent.load(sobject)
+
+   if err then
+      logferror("Could'not deserialize cell object %s", err)
+      return nil
+   end
+
+   return objectfun()
 end
 
 local function setMode(m)
-  mode = m
-  print("push", mode)
-  pushMsg2Threads(mode)
+
+   mode = m
+   print("push", mode)
+   pushMsg2Threads(mode)
 end
 
 local function getMode()
-  return mode
+   return mode
 end
 
 local function doStep()
-  pushMsg2Threads("step")
+   pushMsg2Threads("step")
 end
 
 local function getStatistic()
-  return statistic
+   return statistic
+end
+
+local function getSchema()
+   return mtschema
+end
+
+ Simulator = {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+local function getGridSize()
+   return gridSize
 end
 
 return {
-  create = create,
-  setMode = setMode,
-  getMode = getMode,
-  getDrawList = getDrawList,
-  getDrawLists = getDrawLists,
-  getObject = getObject,
-  step = step,
-  doStep = doStep,
-  getStatistic = getStatistic,
-  getIter = getIter,
-  getGridSize = function()
-    return gridSize
-  end,
-  getSchema = function()
-    return mtschema
-  end,
+   create = create,
+   setMode = setMode,
+   getMode = getMode,
+   getDrawLists = getDrawLists,
+   getObject = getObject,
+   step = step,
+   doStep = doStep,
+   getStatistic = getStatistic,
+   getIter = getIter,
+   getGridSize = getGridSize,
+   getSchema = getSchema,
 }
