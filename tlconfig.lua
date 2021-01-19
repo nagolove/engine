632de8541a7d683e6765c3efd8cd5e19e842f5d7
nagolove <dekar.own@gmail.com>
2021-01-19 20:58:38 +0300

    More Teal modules in automato scene.

--- tlconfig.lua
+++ tlconfig.lua
@@ -2,6 +2,10 @@ return {
     include_dir = {
         "scenes/automato/",
     },
+    include = {
+        "*.tl",
+        "scenes/automato/*.tl",
+    },
     exclude = {
         "*tabular.tl",
         --"scenes/automato/simulator.tl",
