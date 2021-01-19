5c20ec7d4a78450470e06ad76bc520a22eb993ec
nagolove <dekar.own@gmail.com>
2021-01-19 05:26:10 +0300

    Try to compile simulator.tl

--- example.lua
+++ example.lua
@@ -1,5 +1,8 @@
-local inter = require("inter")
-inter.foo()
+local _tl_compat53 = ((tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3) and require('compat53.module'); local ipairs = _tl_compat53 and _tl_compat53.ipairs or ipairs; local pairs = _tl_compat53 and _tl_compat53.pairs or pairs; local pcall = _tl_compat53 and _tl_compat53.pcall or pcall; local inter = require("inter")
+
+
+
+
 
 local Inter = {}
 
@@ -39,3 +42,37 @@ initModule({
 
 
 initModule({})
+
+
+
+local Map = {}
+local Seq = {}
+
+local a = { 1, 2, 3 }
+local b = {
+   hi = -1 or false,
+   lo = 1 or true,
+}
+
+function printMap(m)
+   for k, v in pairs(m) do
+      print(k, v)
+   end
+end
+
+function printSeq(arr)
+   for k, v in ipairs(arr) do
+      print(k, v)
+   end
+end
+
+printSeq(a)
+printMap(b)
+
+
+local ok, errmsg = pcall(function()
+   print("from pcall")
+end)
+
+
+print(ok, errmsg)
--- tlconfig.lua
+++ tlconfig.lua
@@ -1,5 +1,9 @@
 return {
     include_dir = {
         "scenes/automato/",
+    },
+    exclude = {
+        "*tabular.tl",
+        --"scenes/automato/simulator.tl",
     }
 }
