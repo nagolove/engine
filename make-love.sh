43c41698c0fbbf97d9e683374e678314c14f9989
nagolove <dekar.own@gmail.com>
2021-02-01 10:52:09 +0300

    build script.

---
+++ make-love.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+apack game.zip \
+addline2mesh.lua \
+ansicolors.lua \
+assets \
+box2dtool.lua \
+camera.lua \
+cameratool.lua \
+celltool.lua \
+colorselector.lua \
+common.lua \
+compat53 \
+conf.lua \
+debug.lua \
+docks \
+drawing_bitmap.lua \
+external.lua \
+fonts \
+gfx \
+hotkeystool.lua \
+hsx.lua \
+imgui.so \
+inspect.lua \
+inter.lua \
+keyconfig.lua \
+kons.lua \
+list.lua \
+log.lua \
+main.lua \
+matrix.lua \
+menu.lua \
+mobdebug.lua \
+neural_network.lua \
+NeuralNetwork.lua \
+nohup.out \
+particlestool.lua \
+profi.lua \
+qtree.lua \
+render_scene.lua \
+scale.lua \
+scenes/automato/* \
+scenes.lua \
+scenetool.lua \
+serpent.lua \
+ship.lua \
+shiptool.lua \
+struct.lua \
+tabular.lua \
+terrain.lua \
+terraintool.lua \
+Timer.lua \
+tools.lua \
+vector-light.lua \
+vector.lua
+
+mv game.zip game.love
