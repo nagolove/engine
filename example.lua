1f565d7ce2658336352a0a6a6764d838ddf7040b
nagolove <dekar.own@gmail.com>
2021-02-03 06:17:33 +0300

    nope

--- example.lua
+++ example.lua
@@ -1,4 +1,4 @@
-local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local pairs = _tl_compat and _tl_compat.pairs or pairs; local Rec = {}
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local pairs = _tl_compat and _tl_compat.pairs or pairs; local table = _tl_compat and _tl_compat.table or table; local Rec = {}
 
 
 
@@ -34,3 +34,12 @@ function goo(s)
 end
 
 goo(zoo)
+
+
+local t = {
+   "hi",
+   "0",
+   "num",
+}
+
+print(table.concat(t))
