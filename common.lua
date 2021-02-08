cf4773e8f45413ca20ea5a4d76b79f25d31e305e
nagolove <dekar.own@gmail.com>
2021-02-08 16:53:26 +0300

    Teal examples file.

--- common.lua
+++ common.lua
@@ -1,4 +1,5 @@
-local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local load = _tl_compat and _tl_compat.load or load; local pairs = _tl_compat and _tl_compat.pairs or pairs; local string = _tl_compat and _tl_compat.string or string; require("camera")
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local load = _tl_compat and _tl_compat.load or load; local pairs = _tl_compat and _tl_compat.pairs or pairs; local string = _tl_compat and _tl_compat.string or string; require("love")
+require("camera")
 
 function shallowCopy(t)
    local copy = {}
@@ -39,3 +40,17 @@ end
 
 
  Tool = {}
+
+
+
+
+
+
+
+
+
+function safeSend(shader, name, ...)
+   if shader:hasUniform(name) then
+      shader:send(name, (...))
+   end
+end
--- example.lua
+++ example.lua
@@ -1,1 +1,196 @@
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local os = _tl_compat and _tl_compat.os or os; local table = _tl_compat and _tl_compat.table or table; local _tl_table_unpack = unpack or table.unpack
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function foo(n)
+   local t = {}
+   for i = 1, n do
+      table.insert(t, 0)
+   end
+   return _tl_table_unpack(t)
+end
+
+local Date = {}
+
+
+
+
+
+
+
+
+
+
+
+local d = os.date("*t")
+print(d)
+
+
+local Rec = {}
+
+
+
+
+
+local rec_mt
+rec_mt = {
+   __call = function(self, s, n)
+      return tostring(self.x * n) .. s
+   end,
+   __add = function(a, b)
+      local res = setmetatable({}, rec_mt)
+      res.x = a.x + b.x
+      return res
+   end,
+}
