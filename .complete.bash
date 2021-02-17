682dba57422cb086d54d28c8ec26c5cd309c84e2
nagolove <dekar.own@gmail.com>
2021-02-18 01:33:34 +0300

    More engine scripts.

---
+++ .complete.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#complete -W "now tomorrow never" run
+#complete -A /home/testuser/myprojects/engine/scenes run
+#complete -A directory run
+
+_dothis_completions()
+{
+    COMPREPLY=($(compgen -W "$(ls scenes)" "${COMP_WORDS[1]}"))
+}
+
+complete -F _dothis_completions run
