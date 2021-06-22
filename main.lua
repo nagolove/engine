f4919674ad00bb7a6249654aa472a9687e9f0290
nagolove <dekar.own@gmail.com>
2021-06-22 07:19:33 +0300

    Show fps on window header.

--- main.lua
+++ main.lua
@@ -174,7 +174,24 @@ local function collectGarbage()
     end
 end
 
+local lastWindowHeaderUpdateTime = love.timer.getTime()
+local quant = 1
+
+local fpsAccum = 0
+
 function love.update(dt)
+    local now = love.timer.getTime()
+    if now - lastWindowHeaderUpdateTime > quant then
+        love.window.setTitle(love.timer.getFPS())
+    end
+
+    for n, a, b, c, d, e, f in love.event.poll() do
+        print('n, a, b, c, d, e, f', n, a, b, c, d, e, f)
+        --if n == "quit" then
+           ---- Quit!
+        --end	
+    end
+
     if showHelp then
         KeyConfig.updateList(dt)
     end
@@ -265,4 +282,49 @@ function love.wheelmoved(x, y)
   if not imgui.GetWantCaptureMouse() then
     scenes.wheelmoved(x, y)
   end
+end
+
+function love.run()
+    headerUpdateTime = love.timer.getTime()
+	if love.load then love.load(love.arg.parseGameArguments(arg), arg) end
+
+	-- We don't want the first frame's dt to include time taken by love.load.
+	if love.timer then love.timer.step() end
+
+	local dt = 0
+
+	-- Main loop time.
+	return function()
+		-- Process events.
+		if love.event then
+			love.event.pump()
+			for name, a,b,c,d,e,f in love.event.poll() do
+				if name == "quit" then
+					if not love.quit or not love.quit() then
+						return a or 0
+					end
+				end
+				love.handlers[name](a,b,c,d,e,f)
+			end
+		end
+
+		-- Update dt, as we'll be passing it to update
+		if love.timer then dt = love.timer.step() end
+
+        --print('dt', dt)
+
+		-- Call update and draw
+		if love.update then love.update(dt) end -- will pass 0 if love.timer is disabled
+
+		if love.graphics and love.graphics.isActive() then
+			love.graphics.origin()
+			love.graphics.clear(love.graphics.getBackgroundColor())
+
+			if love.draw then love.draw() end
+
+			love.graphics.present()
+		end
+
+		if love.timer then love.timer.sleep(0.001) end
+	end
 end
