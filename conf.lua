9a5c4964a192b3b14c93aed4a5fa0c30ead332b7
nagolove <dekar.own@gmail.com>
2021-07-18 14:14:11 +0000

    config.

--- conf.lua
+++ conf.lua
@@ -1,13 +1,13 @@
 ﻿function love.conf(t)
-    t.window.width = 1920
-    t.window.height = 1080
+    --t.window.width = 1920
+    --t.window.height = 1080
     
     --t.window.width = 800
     --t.window.height = 600
 
     t.window.fullscreen = false
-    --t.window.width = 1024
-    --t.window.height = 768
+    t.window.width = 1024
+    t.window.height = 768
     --t.window.borderless = true         -- Remove all border visuals from the window (boolean)
     t.window.borderless = false         -- Remove all border visuals from the window (boolean)
 
