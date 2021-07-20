89b0c49be24c0e8d6aa9bb39137e22c0ce6b428e
nagolove <dekar.own@gmail.com>
2021-07-20 15:39:22 +0000

    Next compiler examples. Solution in copy-paste style.

--- example2.lua
+++ example2.lua
@@ -1,12 +1,51 @@
 
+local IsPressed = {}
+
+local KeyConfig_ = {BindAccord = {}, Shortcut = {}, }
 
 
 
 
 
- Inter = {}
 
 
 
 
- Inter2 = {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+local Shortcut = KeyConfig_.Shortcut
+local ActionFunc = KeyConfig_.ActionFunc
+
+function KeyConfig_.bind(
+   _,
+   _,
+   _,
+   _,
+   _)
+
+
+end
+
+local Tank = {}
+
+
+
+
+
+
+function Tank:left() end
+function Tank:right() end
+function Tank:down() end
