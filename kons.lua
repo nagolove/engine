822537dae70cfbaa0f5486da9af648bee17ef38c
nagolove <dekar.own@gmail.com>
2021-07-23 18:44:06 +0000

    Konsole custom font.

--- kons.lua
+++ kons.lua
@@ -31,6 +31,13 @@ local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 th
 
 
 
+
+
+
+
+
+
+
 require("love")
 local g = love.graphics
 
@@ -58,9 +65,9 @@ local kons = {Item = {}, }
 
 
 
+
+
 local kons_mt = {}
-
-
 
 
 kons_mt.__index = kons
@@ -69,8 +76,16 @@ function kons_mt.__call(self)
    return self.new()
 end
 
-function kons.new()
+function kons.new(fname, fsize)
+   local font
+   local size = fsize or 20
+   if fname then
+      font = love.graphics.newFont(fname, size)
+   else
+      font = love.graphics.newFont(size)
+   end
    local inst = {
+      font = font,
       color = { 1, 1, 1 },
       show = true,
       strings = {},
@@ -86,6 +101,28 @@ function kons:clear()
    self.strings_i_num = 0
    self.strings = {}
    self.strings_num = 0
+end
+
+function kons:push2(lifetime, text, ...)
+   if type(lifetime) ~= "number" then
+      error("First argument - cardinal value of text lifetime.")
+   end
+
+   local processed = string.gsub(text, "(%%{(.-)})",
+   function(str)
+      print("processing", str)
+      return str
+   end)
+   print("processed", processed)
+
+   assert(lifetime >= 0, string.format("Error: lifetime = %d < 0", lifetime))
+   self.strings[self.strings_num + 1] = {
+      text = string.format(text, ...),
+      lifetime = lifetime,
+      timestamp = love.timer.getTime(),
+   }
+   self.strings_num = self.strings_num + 1
+   return self
 end
 
 function kons:push(lifetime, text, ...)
@@ -117,6 +154,9 @@ function kons:draw(x0, y0)
    local curColor = { g.getColor() }
    g.setColor(self.color)
 
+   local oldFont = love.graphics.getFont()
+   love.graphics.setFont(self.font)
+
    local y = y0
    for k, v in ipairs(self.strings_i) do
       g.print(v, x0, y)
@@ -126,11 +166,11 @@ function kons:draw(x0, y0)
    self.strings_i_num = 0
 
    for _, v in ipairs(self.strings) do
-
       g.print(v.text, x0, y)
       y = y + g.getFont():getHeight()
    end
 
+   love.graphics.setFont(oldFont)
    g.setColor(curColor)
 
    self.height = math.abs(y - y0)
