0f1496fa8d375894eb7b3c6c1ff61dbdcba25dd0
nagolove <dekar.own@gmail.com>
2021-11-20 22:27:13 +0300

    More physics code from Chipmunk Convex demo.

--- physics_wrapper.lua
+++ physics_wrapper.lua
@@ -1,11 +1,11 @@
 local ffi = require 'ffi'
 local colorize = require 'ansicolors2'.ansicolors
 
-require'chipmunk_h'
-local C = ffi.load'chipmunk'
+require 'chipmunk_h'
 
+local C = ffi.load 'chipmunk'
 local concolor = '%{blue}'
-
+local DENSITY = (1.0/10000.0)
 local space
 
 local function init()
@@ -17,8 +17,19 @@ local function init()
 	--cpSpaceSetSleepTimeThreshold(space, 0.5f);
 	--cpSpaceSetCollisionSlop(space, 0.5f);
 
-    -- TODO
-    --local shape = C.cpSpaceAddShape(space, 
+	local width = 50.0
+	local height = 70.0
+	local mass = width * height * DENSITY;
+	local moment = cpMomentForBox(mass, width, height);
+	
+    -- Что такое момент?
+	body = cpSpaceAddBody(space, cpBodyNew(mass, moment));
+    
+    local shape = C.cpSpaceAddShape(space, body)
+
+    -- Что делают строчки ниже?
+	--shape = cpSpaceAddShape(space, cpBoxShapeNew(body, width, height, 0.0));
+	--cpShapeSetFriction(shape, 0.6);
 end
 
 local function update(dt)
