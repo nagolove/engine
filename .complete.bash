4408160ffbcb6495406df58c5bf40fb84593155a
nagolove <dekar.own@gmail.com>
2022-04-18 19:56:05 +0300

    Documentation link.

--- .complete.bash
+++ .complete.bash
@@ -3,6 +3,8 @@
 #complete -A /home/testuser/myprojects/engine/scenes run
 #complete -A directory run
 
+#Как работает этот вызов?
+# https://iridakos.com/programming/2018/03/01/bash-programmable-completion-tutorial
 _dothis_completions()
 {
     COMPREPLY=($(compgen -W "$(ls scenes)" "${COMP_WORDS[1]}"))
