9909a9325e9291e02a3202ad48f64fb8557b38ed
nagolove <dekar.own@gmail.com>
2022-05-22 22:35:23 +0300

    Script for building binaries.

---
+++ premake5.lua
@@ -0,0 +1,47 @@
+workspace "xcaustic"
+    configurations { "Debug", "Release" }
+
+    includedirs { 
+        "/usr/include/luajit-2.1",
+        --"~/projects/Chipmunk2D/include"
+        "/home/nagolove/projects/Chipmunk2D/include",
+        "/home/nagolove/myprojects/lua_capi",
+    }
+    buildoptions { 
+        "-fPIC",
+        "-Wall",
+        "-Werror",
+        "-Wno-strict-aliasing",
+        --"-Wno-unused-function",
+    }
+    links { 
+        --"luajit-5.1", 
+        "lua5.1",
+        "chipmunk",
+        "mem_guard",
+        "lua_tools",
+    }
+    libdirs { 
+        "/home/nagolove/projects/Chipmunk2D/src/",
+        "/home/nagolove/myprojects/c_guard",
+        "/home/nagolove/myprojects/lua_capi",
+    }
+    language "C"
+    kind "SharedLib"
+    --targetdir "bin/%{cfg.buildcfg}"
+    --targetdir "bin/%{cfg.buildcfg}"
+    targetprefix ""
+    targetdir "."
+
+    project "diamond_and_square"
+        files { "**.h", "diamond_and_square.c" }
+        --links { "lua5.1" }
+
+
+    filter "configurations:Debug"
+    defines { "DEBUG" }
+    symbols "On"
+
+    filter "configurations:Release"
+    defines { "NDEBUG" }
+    optimize "On"
